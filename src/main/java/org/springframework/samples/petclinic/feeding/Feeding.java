38044eb7dd5504f6ab7288aac22ad57710169b4a
Pablopben <pablopben@alum.us.es>
2022-01-14 09:55:02 +0100

    5 tests done

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -2,6 +2,18 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.Pet;
 
 import lombok.Getter;
@@ -9,9 +21,26 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Feeding {
-    Integer id;
+@Entity
+@Table(name = "feedings")
+public class Feeding extends BaseEntity{
+    
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    @Column(name = "start_date")
+    @NotEmpty
     LocalDate startDate;
+
+    @Min(1)
+    @NotNull
+    @Column(name = "weeks_duration")
     double weeksDuration;
+
+    @ManyToOne(optional = false)
+    @NotNull
+    @JoinColumn(name = "pet_id")
     Pet pet;   
+
+    @ManyToOne
+    @JoinColumn(name = "feeding_type_id")
+    FeedingType feedingType;
 }
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
@@ -1,5 +1,25 @@
 package org.springframework.samples.petclinic.feeding;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class FeedingController {
+
+    @Autowired
+    public FeedingService feedingService;
+
+    private static final String VIEWS_CREATE_OR_UPDATE_FEEDING_FORM =  "/createOrUpdateFeedingForm";
+
+    @RequestMapping("/feedings/create")
+    public String initCreationForm(ModelMap model){
+
+        Feeding feeding = new Feeding();
+        model.put("feeding", feeding);
+        return VIEWS_CREATE_OR_UPDATE_FEEDING_FORM;
+    }
+
     
 }
