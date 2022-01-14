94c4da99957b3604a21ae2e2d3d72156ac7a5627
Pablopben <pablopben@alum.us.es>
2022-01-14 10:17:50 +0100

    9 tests

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
@@ -1,8 +1,14 @@
 package org.springframework.samples.petclinic.feeding;
 
+import java.lang.ProcessBuilder.Redirect;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,15 +17,34 @@ public class FeedingController {
     @Autowired
     public FeedingService feedingService;
 
-    private static final String VIEWS_CREATE_OR_UPDATE_FEEDING_FORM =  "/createOrUpdateFeedingForm";
+    private static final String VIEWS_CREATE_OR_UPDATE_FEEDING_FORM =  "feedings/createOrUpdateFeedingForm";
 
-    @RequestMapping("/feedings/create")
+    @RequestMapping("/feeding/create")
     public String initCreationForm(ModelMap model){
 
         Feeding feeding = new Feeding();
-        model.put("feeding", feeding);
+        model.addAttribute("feeding", feeding);
+        model.addAttribute("feedingTypes", this.feedingService.getAllFeedingTypes());
+        model.addAttribute("types", this.feedingService.getAll());
         return VIEWS_CREATE_OR_UPDATE_FEEDING_FORM;
     }
+
+    @PostMapping("/feeding/create")
+	public String processCreationForm(@Valid Feeding feeding, BindingResult result, ModelMap model) {		
+		if (result.hasErrors()) {
+			model.put("feeding", feeding);
+			return VIEWS_CREATE_OR_UPDATE_FEEDING_FORM;
+		}
+		else {
+            try{
+                this.feedingService.save(feeding);
+            }catch(UnfeasibleFeedingException ex){
+                result.rejectValue("type", "non conformant", "The selected pet cannot be assigned the specified feeding plan");
+                return VIEWS_CREATE_OR_UPDATE_FEEDING_FORM;
+            }
+            return "redirect:/welcome";
+		}
+	}
 
     
 }
